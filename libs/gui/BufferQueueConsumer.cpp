908cf93a3357f8175416a935295f5245b9100865
1zeroseven4 <zerosevensign@gmail.com>
2025-03-09 19:53:35 +0800

    Revert "IProducerListener: Add a listener for consumer detach"
    This reverts commit 1e7c09b67034f3ff1c263b3c0b5ff0c6d46c6e4a.

--- libs/gui/BufferQueueConsumer.cpp
+++ libs/gui/BufferQueueConsumer.cpp
@@ -328,9 +328,7 @@ status_t BufferQueueConsumer::detachBuffer(int slot) {
     ATRACE_CALL();
     ATRACE_BUFFER_INDEX(slot);
     BQ_LOGV("detachBuffer: slot %d", slot);
-    sp<IProducerListener> listener;
-    {
-        std::lock_guard<std::mutex> lock(mCore->mMutex);
+    std::lock_guard<std::mutex> lock(mCore->mMutex);
 
         if (mCore->mIsAbandoned) {
             BQ_LOGE("detachBuffer: BufferQueue has been abandoned");
@@ -368,9 +366,28 @@ status_t BufferQueueConsumer::detachBuffer(int slot) {
         VALIDATE_CONSISTENCY();
     }
 
-    if (listener) {
-        listener->onBufferDetached(slot);
+    if (mCore->mSharedBufferMode || slot == mCore->mSharedBufferSlot) {
+        BQ_LOGE("detachBuffer: detachBuffer not allowed in shared buffer mode");
+        return BAD_VALUE;
     }
+
+    if (slot < 0 || slot >= BufferQueueDefs::NUM_BUFFER_SLOTS) {
+        BQ_LOGE("detachBuffer: slot index %d out of range [0, %d)",
+                slot, BufferQueueDefs::NUM_BUFFER_SLOTS);
+        return BAD_VALUE;
+    } else if (!mSlots[slot].mBufferState.isAcquired()) {
+        BQ_LOGE("detachBuffer: slot %d is not owned by the consumer "
+                "(state = %s)", slot, mSlots[slot].mBufferState.string());
+        return BAD_VALUE;
+    }
+
+    mSlots[slot].mBufferState.detachConsumer();
+    mCore->mActiveBuffers.erase(slot);
+    mCore->mFreeSlots.insert(slot);
+    mCore->clearBufferSlotLocked(slot);
+    mCore->mDequeueCondition.notify_all();
+    VALIDATE_CONSISTENCY();
+
     return NO_ERROR;
 }
 
